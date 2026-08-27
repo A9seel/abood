8bd25444bfe27b632550b9b475587e4be158941b
A9seel <spookyspar47@gmail.com>
2026-08-27 20:59:03 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+   // نستخدم querySelectorAll لالتقاط جميع الأشرطة التي تحمل نفس الكلاس
+const containers = document.querySelectorAll('.scroll-container');
+
+containers.forEach((container) => {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    container.addEventListener('mousedown', (e) => {
+        isDown = true;
+        container.style.cursor = 'grabbing';
+        startX = e.pageX - container.offsetLeft;
+        scrollLeft = container.scrollLeft;
+    });
+
+    container.addEventListener('mouseleave', () => {
+        isDown = false;
+        container.style.cursor = 'grab';
+    });
+
+    container.addEventListener('mouseup', () => {
+        isDown = false;
+        container.style.cursor = 'grab';
+    });
+
+    container.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - container.offsetLeft;
+        const walk = (x - startX) * 2; 
+        container.scrollLeft = scrollLeft - walk;
+    });
+});
+
+
+
